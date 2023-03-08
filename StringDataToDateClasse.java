84d98b81f2ca80f2d946f0353bed8a7328653a56
Weber Ribeiro Marcondes <70358508+webermarcondes@users.noreply.github.com>
2023-03-08 00:44:46 -0300

    Conversão Data em String para Date
    Conversão de um data fornecida em texto para um tipo válido.

---
+++ StringDataToDateClasse.java
@@ -0,0 +1,23 @@
+package StringDataToDate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringDataToDateClasse {
+
+	public static void main(String args[]) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		String dataStr = "07/03/2023 21:47:15";		
+		Date data = null;
+		try {
+			data = sdf.parse(dataStr);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(data);
+	}
+}
